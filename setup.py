9794a78e217a4952825d61224319ad661e8ee7c3
Yuji Tomita <yuchant@gmail.com>
2011-12-15 20:30:19 -0800

    adding all * to manifest

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="django-cleditor",
     version='0.1',
-    description="A django form widget for CLEditor (a super clean, MIT licensed WYSIWYG HTML editor",
+    description="A django form widget for CLEditor (a super clean, MIT licensed WYSIWYG HTML editor)",
     long_description='',
     author="Yuji Tomita",
     author_email="yuji@yujitomita.com",
     url="https://github.com/yuchant/django-cleditor",
+    packages=['cleditor',],
+    package_dir={'cleditor': 'cleditor'},
     include_package_data=True,
-    packages=find_packages(),
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
