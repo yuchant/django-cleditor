db5741fe1e69ae2ee7cf42a17779ba3f2c705668
Yuji Tomita <yuchant@gmail.com>
2011-12-29 11:13:25 -0800

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
 setup(
     name="django-cleditor",
-    version='0.1',
+    version='0.1.5',
     description="A django form widget for CLEditor (a super clean, MIT licensed WYSIWYG HTML editor)",
     long_description='',
     author="Yuji Tomita",
     author_email="yuji@yujitomita.com",
     url="https://github.com/yuchant/django-cleditor",
-    packages=find_packages(),
+    packages=[
+        'cleditor',
+    ],
+    package_dir={
+      'cleditor': 'cleditor',
+    },
     include_package_data=True,
     classifiers=[
         "Environment :: Web Environment",
